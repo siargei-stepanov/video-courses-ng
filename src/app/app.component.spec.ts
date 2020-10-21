2545560788c7660346aa9850515f844690e351c0
Sergey Stepanov <siargei.stepanov@gmail.com>
2020-10-21 21:41:54 +0300

    Fixed tests from previous tasks

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,17 +19,4 @@ describe('AppComponent', () => {
 		const app = fixture.componentInstance;
 		expect(app).toBeTruthy();
 	});
-
-	it(`should have as title 'video-courses-ng'`, () => {
-		const fixture = TestBed.createComponent(AppComponent);
-		const app = fixture.componentInstance;
-		expect(app.title).toEqual('video-courses-ng');
-	});
-
-	it('should render title', () => {
-		const fixture = TestBed.createComponent(AppComponent);
-		fixture.detectChanges();
-		const compiled = fixture.nativeElement;
-		expect(compiled.querySelector('.content span').textContent).toContain('video-courses-ng app is running!');
-	});
 });
--- src/app/courses/course-item/course-item.component.spec.ts
+++ src/app/courses/course-item/course-item.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Course } from '../course.model';
 
 import { CourseItemComponent } from './course-item.component';
 
@@ -16,6 +17,7 @@ describe('CourseItemComponent', () => {
 	beforeEach(() => {
 		fixture = TestBed.createComponent(CourseItemComponent);
 		component = fixture.componentInstance;
+		component.course = new Course(1, 'title', '2020-01-01', 60, 'description');
 		fixture.detectChanges();
 	});
 
