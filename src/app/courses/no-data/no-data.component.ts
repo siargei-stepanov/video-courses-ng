748795f388404f3c6a66b6950d99080b56f9b8c7
Sergey Stepanov <siargei.stepanov@gmail.com>
2020-10-29 21:38:03 +0300

    Added no data component

---
+++ src/app/courses/no-data/no-data.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+	selector: 'app-no-data',
+	templateUrl: './no-data.component.html',
+	styleUrls: ['./no-data.component.less']
+})
+export class NoDataComponent implements OnInit {
+
+	constructor() {
+	}
+
+	ngOnInit(): void {
+	}
+
+}
