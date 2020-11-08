eaa6b31ba8dc710b737a99d8ecb96fe4fe1f995f
Sergey Stepanov <siargei.stepanov@gmail.com>
2020-11-08 17:41:58 +0300

    Replace hardcode courses with service call

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CoursesService } from '../common/courses.service';
 import { Course } from './course.model';
 import { FilterPipe } from './filter.pipe';
 
@@ -12,10 +13,10 @@ export class CoursesComponent implements OnInit {
 	private filterPipe = new FilterPipe();
 	private courses: Course[];
 
-	constructor() {}
+	constructor(private coursesService: CoursesService) {}
 
 	ngOnInit(): void {
-		this.filteredCourses = this.courses = this.getCourses();
+		this.filteredCourses = this.courses = this.coursesService.getList();
 	}
 
 	public editCourse(id: number): void {
@@ -32,35 +33,5 @@ export class CoursesComponent implements OnInit {
 
 	public searchCourse(query: string): void {
 		this.filteredCourses = this.filterPipe.transform(this.courses, query);
-	}
-
-	private getCourses(): Course[] {
-		return [
-			{
-				id: 1,
-				title: 'Angular Mentoring Program',
-				duration: 600,
-				topRated: true,
-				description: 'Overview of Angular with lectures and homework.',
-				creationDate: '2020-10-20',
-			},
-			{
-				id: 2,
-				title: 'NodeJS Mentoring Program',
-				duration: 740,
-				topRated: false,
-				description:
-					'Overview of NodeJS with lectures and homework. Event loops and stuff.',
-				creationDate: '2020-12-01',
-			},
-			{
-				id: 3,
-				title: 'HTML/CSS Mentoring Program',
-				duration: 260,
-				topRated: false,
-				description: 'HTML and CSS courses for IT newcomers.',
-				creationDate: '2020-09-01',
-			},
-		];
 	}
 }
