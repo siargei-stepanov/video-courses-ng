5cae8ca9152ca7dd80f565669652723aa7e9a779
Sergey Stepanov <siargei.stepanov@gmail.com>
2020-11-09 21:29:41 +0300

    Added login page skeleton

--- src/app/login-page/login-page.module.ts
+++ src/app/login-page/login-page.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginPageComponent } from './login-page.component';
 import { LoginPageRoutingModule } from './login-page-routing.module';
+import { HeaderModule } from '../header/header.module';
+import { FooterModule } from '../footer/footer.module';
 
 @NgModule({
 	declarations: [LoginPageComponent],
-	imports: [CommonModule, LoginPageRoutingModule],
+	imports: [CommonModule, LoginPageRoutingModule, HeaderModule, FooterModule],
 })
 export class LoginPageModule {}
