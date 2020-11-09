ef3110ed08e438575308b4edf42c2d86aaa564a4
Sergey Stepanov <siargei.stepanov@gmail.com>
2020-11-09 21:03:49 +0300

    Added empty login page

---
+++ src/app/login-page/login-page.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginPageComponent } from './login-page.component';
+import { LoginPageRoutingModule } from './login-page-routing.module';
+
+@NgModule({
+	declarations: [LoginPageComponent],
+	imports: [CommonModule, LoginPageRoutingModule],
+})
+export class LoginPageModule {}
