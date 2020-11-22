d090adae434eb324216a84e694f4e1100768e86f
Sergey Stepanov <siargei.stepanov@gmail.com>
2020-11-22 19:54:09 +0300

    Added/changed routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,24 +4,40 @@ import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 const routes: Routes = [
 	{
 		path: 'courses',
+		pathMatch: 'full',
 		loadChildren: () =>
-			import('./pages/list-page/list-page.module').then((m) => m.ListPageModule),
+			import('./pages/list-page/list-page.module').then(
+				(m) => m.ListPageModule
+			),
 	},
 	{
 		path: 'courses/new',
 		loadChildren: () =>
-			import('./pages/add-course-page/add-course-page.module').then((m) => m.AddCoursePageModule),
+			import('./pages/add-course-page/add-course-page.module').then(
+				(m) => m.AddCoursePageModule
+			),
+	},
+	{
+		path: 'courses/:id',
+		loadChildren: () =>
+			import('./pages/add-course-page/add-course-page.module').then(
+				(m) => m.AddCoursePageModule
+			),
 	},
 	{
 		path: 'login',
 		loadChildren: () =>
-			import('./pages/login-page/login-page.module').then((m) => m.LoginPageModule),
+			import('./pages/login-page/login-page.module').then(
+				(m) => m.LoginPageModule
+			),
 	},
 	{ path: '', redirectTo: '/courses', pathMatch: 'full' },
 	{
 		path: '**',
 		loadChildren: () =>
-			import('./pages/not-found-page/not-found-page.module').then((m) => m.NotFoundPageModule),
+			import('./pages/not-found-page/not-found-page.module').then(
+				(m) => m.NotFoundPageModule
+			),
 	},
 ];
 
