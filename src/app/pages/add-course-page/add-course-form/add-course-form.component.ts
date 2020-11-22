f74164bdcb2409c75e413d65b047b8f03433725b
Sergey Stepanov <siargei.stepanov@gmail.com>
2020-11-22 19:56:48 +0300

    Added navigations between pages

--- src/app/pages/add-course-page/add-course-form/add-course-form.component.ts
+++ src/app/pages/add-course-page/add-course-form/add-course-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Course } from 'src/app/common/course.model';
+import { CoursesService } from 'src/app/common/services/courses.service';
 
 @Component({
 	selector: 'app-add-course-form',
@@ -6,15 +9,28 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./add-course-form.component.less'],
 })
 export class AddCourseFormComponent implements OnInit {
-	constructor() {}
+	public course: Course;
 
-	ngOnInit(): void {}
+	constructor(private route: ActivatedRoute, private courseService: CoursesService, private router: Router) {}
+
+	ngOnInit(): void {
+		this.route.paramMap.subscribe((routeParams) => {
+			const id = parseInt(routeParams.get('id'), 10);
+			const newCourse = new Course(null, '', '', 0, '', false);
+			if (isNaN(id)) {
+				this.course = newCourse;
+			} else {
+				const courseFromService = this.courseService.getById(id);
+				this.course = courseFromService ? courseFromService : newCourse;
+			}
+		});
+	}
 
 	public onSave(): void {
 		console.log('add course. on save');
 	}
 
 	public onCancel(): void {
-		console.log('add course. on cancel');
+		this.router.navigateByUrl('/courses');
 	}
 }
